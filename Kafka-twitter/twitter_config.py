92015535247bc484b801002c2822788b4f31d1bc
Changsheng <zxdmike@163.com>
2018-04-24 16:10:08 -0400

    kafka

---
+++ Kafka-twitter/twitter_config.py
@@ -0,0 +1,7 @@
+#These information comes from Twitter API
+#Create a Twitter Account and Get These Information from apps.twitter.com
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
