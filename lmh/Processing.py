87f1f7c4206f916657655038df7f4f19016bfb03
Jimenius <l.minhui@qq.com>
2018-04-26 20:02:44 -0400

    ProcINIT

--- lmh/Processing.py
+++ lmh/Processing.py
@@ -16,7 +16,7 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 import json
-import operator
+from operator import add
 from textblob import TextBlob
 import matplotlib.pyplot as plt
 
@@ -45,6 +45,36 @@ def make_plot(counts):
     plt.legend(loc = 'upper left')
     plt.show()
 
+def makePlot(sentiment_list):
+    sentiment_count = [0] * 3
+    sentiment_count[sentiment_list[0]] = sentiment_list[1]
+    sentiment_count[sentiment_list[2]] = sentiment_list[3]
+    sentiment_count[sentiment_list[4]] = sentiment_list[5]
+    plt.figure()
+    plt.plot(sentiment_count)
+    plt.savefig('img_lib/count.png')
+    plt.close()
+
+def clean_tweet(tweet):
+    '''
+    Utility function to clean the text in a tweet by removing 
+    links and special characters using regex.
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+def analize_sentiment(tweet):
+    '''
+    Utility function to classify the polarity of a tweet
+    using textblob.
+    '''
+    analysis = TextBlob(clean_tweet(tweet))
+    if analysis.sentiment.polarity > 0:
+        return 1
+    elif analysis.sentiment.polarity == 0:
+        return 0
+    else:
+        return -1
+
 def sendRecord(record):
     connection = createNewConnection()
     connection.send(record)
@@ -58,8 +88,10 @@ if __name__ == "__main__":
     sc = SparkContext(appName = 'NewsTwitter')
     ssc = StreamingContext(sc, 10)
     kafkaStream = KafkaUtils.createStream(ssc, 'localhost:2181', 'spark-streaming', {'twitter':1})
-    parsed = kafkaStream.map(lambda v: json.loads(v[1]))
-    words = parsed.flatMap(lambda line:line['text'].split(" "))
+    parsed = kafkaStream.map(lambda v: json.loads(v[1])).map(lambda line: line['text'])
+    sentiment = parsed.map(lambda tweet: (analize_sentiment(tweet), 1))
+    sentiment_list = sentiment.reduceByKey(add).map(lambda x, y: [x, y]).reduce(x, y: x + y)
+    sentiment_list.map(makePlot)
 
     ssc.start()
     ssc.awaitTerminationOrTimeout(30)
